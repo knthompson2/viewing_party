c8f5f16be9b8df6df8bdfb63d3a1130bb86fa542
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-10-20 17:59:06 -0500

    Add wallpaper

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,13 +1,13 @@
 class MovieFacade
   def self.top_40_movies
-    movies = MovieService.top_rated_movies('1') + MovieService.top_rated_movies('2')
+    movies = MovieService.top_rated_movies('1')[:results] + MovieService.top_rated_movies('2')[:results]
     movies.map do |movie_data|
       Movie.new(movie_data)
     end
   end
 
   def self.search_by_title(title)
-    movies = MovieService.movie_search(title, 1) + MovieService.movie_search(title, 2)
+    movies = MovieService.movie_search(title, 1)[:results] + MovieService.movie_search(title, 2)[:results]
     movies.map do |movie_data|
       Movie.new(movie_data)
     end
@@ -19,21 +19,21 @@ class MovieFacade
   end
 
   def self.cast_members(movie_id)
-    data = MovieService.get_cast_members(movie_id)
+    data = MovieService.get_cast_members(movie_id)[:cast]
     data[0..9].map do |actor_data|
       Actor.new(actor_data)
     end
   end
 
   def self.movie_reviews(movie_id)
-    reviews = MovieService.get_reviews(movie_id)
+    reviews = MovieService.get_reviews(movie_id)[:results]
     reviews.map do |review_data|
       Review.new(review_data)
     end
   end
 
   def self.upcoming_movies
-    upcoming = MovieService.upcoming
+    upcoming = MovieService.upcoming[:results]
 
     upcoming.map do |movie|
       Movie.new(movie)
@@ -41,7 +41,7 @@ class MovieFacade
   end
 
   def self.similar_movies(movie_id)
-    similar = MovieService.similar(movie_id)
+    similar = MovieService.similar(movie_id)[:results]
 
     similar.map do |movie|
       Movie.new(movie)
--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -3,42 +3,50 @@ require 'rails_helper'
 RSpec.describe MovieService do
   it 'returns a list of 20 top rated movies by page number', :vcr do
     top_movies = MovieService.top_rated_movies(1)
+    top_movies_results = MovieService.top_rated_movies(1)[:results]
 
-    expect(top_movies).to be_a(Array)
-    expect(top_movies.first).to have_key(:id)
-    expect(top_movies.first).to have_key(:title)
-    expect(top_movies.first).to have_key(:overview)
-    expect(top_movies.first).to have_key(:vote_average)
-    expect(top_movies.first).to have_key(:poster_path)
-    expect(top_movies.count).to eq(20)
+    expect(top_movies).to be_a(Hash)
+    expect(top_movies_results.first).to have_key(:id)
+    expect(top_movies_results.first[:id]).to be_an(Integer)
+    expect(top_movies_results.first).to have_key(:title)
+    expect(top_movies_results.first[:title]).to be_a(String)
+    expect(top_movies_results.first).to have_key(:overview)
+    expect(top_movies_results.first[:overview]).to be_a(String)
+    expect(top_movies_results.first).to have_key(:vote_average)
+    expect(top_movies_results.first[:vote_average]).to be_an(Float)
+    expect(top_movies_results.first).to have_key(:poster_path)
+    expect(top_movies_results.first[:poster_path]).to be_a(String)
+    expect(top_movies_results.count).to eq(20)
   end
 
   it 'returns an array of movie reviews', :vcr do
     movie_review = MovieService.get_reviews(238)
+    movie_review_results = MovieService.get_reviews(238)[:results]
 
-    expect(movie_review).to be_a(Array)
-    expect(movie_review.first).to have_key(:author)
-    expect(movie_review.first).to have_key(:content)
+    expect(movie_review).to be_a(Hash)
+    expect(movie_review_results.first).to have_key(:author)
+    expect(movie_review_results.first).to have_key(:content)
   end
 
   it 'returns an array of movie cast members', :vcr do
     cast_members = MovieService.get_cast_members(238)
+    cast_members_results = MovieService.get_cast_members(238)[:cast]
 
-    expect(cast_members).to be_a(Array)
-    expect(cast_members.first).to have_key(:name)
-    expect(cast_members.first).to have_key(:character)
+    expect(cast_members).to be_a(Hash)
+    expect(cast_members_results.first).to have_key(:name)
+    expect(cast_members_results.first).to have_key(:character)
   end
 
   it 'returns a list of movies by title search by page number', :vcr do
     title_search = MovieService.movie_search("The", 1)
 
-    expect(title_search).to be_a(Array)
-    expect(title_search.first).to have_key(:id)
-    expect(title_search.first).to have_key(:title)
-    expect(title_search.first).to have_key(:overview)
-    expect(title_search.first).to have_key(:vote_average)
-    expect(title_search.first).to have_key(:poster_path)
-    expect(title_search.count).to eq(20)
+    expect(title_search).to be_a(Hash)
+    expect(title_search[:results].first).to have_key(:id)
+    expect(title_search[:results].first).to have_key(:title)
+    expect(title_search[:results].first).to have_key(:overview)
+    expect(title_search[:results].first).to have_key(:vote_average)
+    expect(title_search[:results].first).to have_key(:poster_path)
+    expect(title_search.count).to eq(4)
   end
 
   it 'returns a movie by movie id', :vcr do
@@ -53,19 +61,21 @@ RSpec.describe MovieService do
 
   it 'returns upcoming movies', :vcr do
     upcoming = MovieService.upcoming
+    upcoming_movies = MovieService.upcoming[:results]
 
-    expect(upcoming).to be_a(Array)
-    expect(upcoming.first).to have_key(:id)
-    expect(upcoming.first).to have_key(:title)
-    expect(upcoming.count).to eq(20)
+    expect(upcoming).to be_a(Hash)
+    expect(upcoming_movies.first).to have_key(:id)
+    expect(upcoming_movies.first).to have_key(:title)
+    expect(upcoming_movies.count).to eq(20)
   end
 
   it 'returns similar movies', :vcr do
     similar = MovieService.similar(238)
+    similar_movies = MovieService.similar(238)[:results]
 
-    expect(similar).to be_a(Array)
-    expect(similar.first).to have_key(:id)
-    expect(similar.first).to have_key(:title)
-    expect(similar.count).to eq(20)
+    expect(similar).to be_a(Hash)
+    expect(similar_movies.first).to have_key(:id)
+    expect(similar_movies.first).to have_key(:title)
+    expect(similar.count).to eq(4)
   end
 end
